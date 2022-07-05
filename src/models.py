c91bceb2caf571d732568658aadf434af1cf2478
herucoro <herucoro3@gmail.com>
2022-07-06 00:37:50 +0900

    commit

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ from database import Base
 import sqlalchemy as sa
 
 class User(Base):
-    __table__ = 'users'
+    __tablename__ = 'users'
     user_id = sa.Column('user_id', sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column('name', sa.Text, nullable=False)
     age = sa.Column('age', sa.Integer, nullable=False)
